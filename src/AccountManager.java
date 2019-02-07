b57769f85c1ca100a89b9c988461196ebeecf368
manoj <manoj.sarwate@ssiedu.in>
2019-02-07 17:33:48 +0530

    07-02-19

--- src/AccountManager.java
+++ src/AccountManager.java
@@ -9,12 +9,21 @@ public static void main(String[] args) {
     ac2.setData(112,"BBB",20000);
     ac3.setData(113,"CCC",30000);
     
+    
+    //BankAccount.showData(ac1);
+    //BankAccount.showData(ac2);
+    //BankAccount.showData(ac3);
     ac1.showData();
     ac2.showData();
     ac3.showData();
+    BankAccount.showTotalCash();
     
-    ac1.showTotalCash();
-    ac2.showTotalCash();
-    ac3.showTotalCash();
+    //ac3.showAvg(ac1);
+    //ac2.showAvg(ac3);
+    
+    BankAccount.showAvg(ac1,ac3);
+    BankAccount.showAvg(ac2,ac3);
+    
+    
 }
 }
--- src/BankAccount.java
+++ src/BankAccount.java
@@ -4,25 +4,25 @@ public class BankAccount {
     private String cname;
     private int balance;
     private static int totalCash;
-    
-    public void showTotalCash(){
+
+    public static void showAvg(BankAccount tmp1, BankAccount tmp2){
+        //avg of any 2 accounts
+        System.out.println("Avg of 2 : "+(tmp1.balance+tmp2.balance)/2);
+    }
+    public static void showTotalCash(){
         System.out.println("TOTAL CASH  : "+totalCash);
         System.out.println("==================================");
     }
-    
-    
     public void showData(){
         System.out.println("Account Number : "+ano);
         System.out.println("Customer Name  : "+cname);
         System.out.println("Balance    Rs. : "+balance);
         System.out.println("_______________________________________________");
     }
-    
     public void setData(int a, String b, int c){
         ano=a;
         cname=b;
         balance=c;
         totalCash=totalCash+c;
     }
-    
 }
